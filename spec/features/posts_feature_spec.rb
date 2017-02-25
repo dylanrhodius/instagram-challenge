1227ea43385a6214e74e363e42d59605b0db59bb
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-02-25 17:08:27 +0000

    TDD displaying posts

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 feature 'image posts' do
+
   context 'no image posts have been added' do
     scenario 'should display a prompt to add an image post' do
       visit '/posts'
@@ -8,4 +9,18 @@ feature 'image posts' do
       expect(page).to have_link 'Post a picture'
     end
   end
+
+  context 'posts have been added' do
+    before do
+      Post.create(name: 'post')
+    end
+
+    scenario 'display posts' do
+      visit '/posts'
+      expect(page).to have_content('Post')
+      expect(page).not_to have_content('No posts yet')
+    end
+  end
+
+
 end
