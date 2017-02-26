eb0b114bc0d6f0345e6964b9e20b3f1d20365940
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-02-26 16:43:47 +0000

    Revert last commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
-  validates :caption, length: { minimum: 1 }, uniqueness: true
+  validates :caption, length: { minimum: 1 }
 end
