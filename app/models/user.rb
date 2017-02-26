b469930ae5ba9d5b8ed2d9ffab1699eb61f964e1
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-02-26 22:58:27 +0000

    trying to make index better

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+
+  has_many :comments, dependent: :destroy
+  has_many :posts, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -24,4 +28,9 @@ class User < ApplicationRecord
        end
      end
    end
+
+   def owns?(post)
+     posts.include? post
+   end
+
 end
